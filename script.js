f2b4164cef46a1db28483cd8baa4b9abb5202957
David Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2023-09-21 09:41:04 +0100

    Created a for loop to loop through posts array. Creating elements and appending them to sub-container  class.

--- script.js
+++ script.js
@@ -30,5 +30,65 @@ const posts = [
     }
 ];
 
-// 1 ) loop through array
-// 2 ) create elements and append them to parents + textContent.
+// 1) create element
+
+// 2) textContent element
+
+// 3) append element 
+
+// 4) add class to element
+
+const subContainer = document.querySelector('.sub-container');
+
+
+
+
+for(let i = 0; i < posts.length; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    subContainer.append(row);
+    
+    const column = document.createElement('div');
+    column.className = 'column';
+    row.append(column);
+
+    const img = document.createElement('img');
+    img.className = 'small-img';
+    img.src = posts[i].avatar;
+    img.alt = posts[i].name;
+    row.append(img);
+
+    let boldPara = document.createElement('p');
+    boldPara.className = 'bold-para';
+    const smallPara = document.createElement('p');
+    smallPara.className = 'small-para';
+    boldPara.textContent = posts[i].name;
+    smallPara.textContent = posts[i].location;
+    column.append(boldPara);
+    column.append(smallPara);
+
+    const mainImg = document.createElement('img');
+    mainImg.className = 'main-img';
+    mainImg.src = posts[i].post;
+    mainImg.alt = posts[i].name;
+    subContainer.append(mainImg);
+
+    // const icons = document.createElement('img');
+    // icons.className = 'img-icons';
+    // icons.src = 'images/icon-heart.png';
+    // icons.alt = 'heart-icon';
+    // subContainer.append(icons)
+
+
+
+}
+
+const iconImgs = ['images/icon-heart.png', 'images/icon-heart.png', 'images/icon-heart.png']
+
+function renderIconImgs() {
+    const icons = document.createElement('img');
+    icons.className = 'img-icons';
+    icons.src = 'images/icon-heart.png';
+    icons.alt = 'heart-icon';
+    subContainer.append(icons)
+}
